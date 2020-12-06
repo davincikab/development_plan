b92d0ea878ef24d2b6f63821c5d9985fd8496b41
davincikab <davidnganganjeri079@gmail.com>
2020-12-07 01:15:34 +0300

    development models

--- development/models.py
+++ development/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Boundary(models.Model):
+    geom = models.MultiLineStringField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'boundary'
+
+
+class Parcels(models.Model):
+    geom = models.MultiPolygonField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    zone = models.CharField(max_length=254, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'parcels'
+
+
+class Rivers(models.Model):
+    geom = models.MultiLineStringField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'rivers'
+
+
+class Roads(models.Model):
+    geom = models.MultiLineStringField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    cat = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'roads'
