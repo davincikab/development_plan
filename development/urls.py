ad372f89a12b9ceae69ab97f10173d3aeda645cf
davincikab <davidnganganjeri079@gmail.com>
2020-12-07 07:08:27 +0300

    data view and urls

--- development/urls.py
+++ development/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main_view
+from .views import main_view, data_view
 
 urlpatterns = [
-      path('', main_view, name='home'),
+    path('', main_view, name='home'),
+    path('data/', data_view, name='data')
 ]
--- development/views.py
+++ development/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.core.serializers import serialize
 
-# from .models import Alert
+from .models import Boundary, Parcels, Rivers, Roads
 
 # Create your views here.
 # @login_required
 def main_view(request):
     return render(request, 'development/index.html')
+
+def data_view(request):
+    context = {
+        'rivers':serialize('geojson', Rivers.objects.all()),
+        'roads':serialize('geojson', Roads.objects.all()),
+        'boundary':serialize('geojson', Boundary.objects.all()),
+        'parcels':serialize('geojson', Parcels.objects.all())
+    }
+    
+    return JsonResponse(context)
