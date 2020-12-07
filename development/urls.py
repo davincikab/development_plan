eef3d22125596413e77444972175ffe4f73a83e3
davincikab <davidnganganjeri079@gmail.com>
2020-12-07 07:04:05 +0300

    development template files

--- development/urls.py
+++ development/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path,
+from django.urls import path
+from .views import main_view
 
 urlpatterns = [
-    path('', )
+      path('', main_view, name='home'),
 ]
--- development/views.py
+++ development/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+
+# from .models import Alert
 
 # Create your views here.
+# @login_required
+def main_view(request):
+    return render(request, 'development/index.html')
